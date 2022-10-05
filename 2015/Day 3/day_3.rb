afa329b01c9ccd589bd375a3bd360e4cea1d3a5b
Chris Minns <chrism465@gmail.com>
2022-10-05 17:42:30 +0100

    same again

--- 2015/Day 3/day_3.rb
+++ 2015/Day 3/day_3.rb
@@ -2,12 +2,39 @@ require_relative 'house'
 
 class Day3
   def initialize
-    x = House.new
-    p x[:north]
+    x = "^>v<".chars
+    # journey(x)
+    test
+  end
+
+  def opposite_direction(char)
+    { "^" => "v", "v" => "^", "<" => ">", ">" => "<" }[char]
+  end
+
+  def journey(magical)
+    acc = 0
+    first_house = House.new
+    current_house = first_house
+    magical.each do |i|
+      if current_house[i].nil?
+        temp = House.new()
+        temp[opposite_direction(i)] = current_house
+        current_house = temp
+        acc += 1
+      else
+        current_house = current_house[i]
+      end
+    end
+    acc
+  end
+
+  def test
+    temp = House.new
+    temp["^"] = "bones"
+    p temp["^"]
   end
 end
 
 Day3.new
-
-#Instead, I would suggest having a normal class that only uses a hash as an internal data structure (which could be easily and idiomatically exposed with a to_h method).
+# Instead, I would suggest having a normal class that only uses a hash as an internal data structure (which could be easily and idiomatically exposed with a to_h method).
 
--- 2015/Day 3/house.rb
+++ 2015/Day 3/house.rb
@@ -1,9 +1,39 @@
-class House < Hash
-  def initialize
-    super 
-    self[:north] = 'bones'
-    self[:south] = 'beans'
-    self[:east] = 'jeans'
-    self[:west] = 'jones'
+class House
+  attr_accessor :north, :south, :east, :west
+
+  def initialize(north: nil, south: nil, east: nil, west: nil)
+    @north = north
+    @south = south
+    @east = east
+    @west = west
+    @this = { north: north, south: south, east: east, west: west }
+  end
+
+  def to_h
+    @this
+  end
+
+  def ^
+    north
+  end
+
+  def v
+    south
+  end
+
+  def <
+    west
+  end
+
+  def >
+    east
+  end
+
+  def [](key)
+    { "^" => north, "v" => south, ">" => east, "<" => west }[key]
+  end
+
+  def []=(key, value)
+    @this[{ "^" => north, "v" => south, ">" => east, "<" => west }[key]] = value
   end
 end
